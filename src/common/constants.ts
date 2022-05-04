72e002c98d5a13aee2d41ef2ded6a6b0c572854a
RoanArraes <roan.arraes@hotmail.com>
2022-05-03 21:27:49 -0300

    insert new constants

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,11 +1,3 @@
-export const BARGRAPH_INDICATORS_LABEL = {
-  ATTACK: "ATK",
-  DEFENSE: "DEF",
-  SPEED: "SPD",
-  HP: "HP",
-  SPECIAL_ATTACK: "SP-ATK"
-};
-
 export const BUTTON_CENTRAL_DIRECTIONS = {
   UP: "UP",
   DOWN: "DOWN",
@@ -15,4 +7,25 @@ export const BUTTON_CENTRAL_DIRECTIONS = {
 
 export const BUTTON_SELECT = {
   SELECT: "SELECT"
+};
+
+//DISPLAY CARACTERISTICS COMPONENT
+export const BASE_STAT_OBJECT_POKEMON_DATA = "base_stat";
+
+export const SENSIBILITY_BARGRAPH = 0.7;
+
+export const OPTIONS_OBJECT_POKEMON_DATA = {
+  HP: "hp",
+  ATTACK: "attack",
+  DEFENSE: "defense",
+  SPEED: "speed",
+  SP_ATTACK: "special-attack",
+};
+
+export const BARGRAPH_INDICATORS_LABEL = {
+  ATTACK: "ATK",
+  DEFENSE: "DEF",
+  SPEED: "SPD",
+  HP: "HP",
+  SPECIAL_ATTACK: "SP-ATK"
 };
