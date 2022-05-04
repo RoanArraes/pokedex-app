bcc09f92136aa7507422ba01ae5bd99de1f947dc
RoanArraes <roan.arraes@hotmail.com>
2022-05-03 21:26:35 -0300

    adjust to dumb component

--- src/components/image-render/ImageRender.tsx
+++ src/components/image-render/ImageRender.tsx
@@ -1,36 +1,19 @@
-import { useEffect, useState } from 'react';
-import { getPokemonData } from '../../services/pokemons';
 import {
   Image
 } from './styles';
 
 type Props = {
-  idToGetImage: string
+  urlImage: string
 }
 
 const ImageRender = ({
-  idToGetImage
+  urlImage
 }: Props) => {
-
-  useEffect(() => {
-    if(idToGetImage) {
-      getImagePokemon(idToGetImage)
-    }
-  }, [idToGetImage]);
-
-  const [ image, setImage ] = useState("");
-
-  const getImagePokemon = async (id: string) => {
-    const response = await getPokemonData(id);
-    if (response) {
-      setImage(response.sprites["front_default"]);
-    }
-  }
 
   return (
     <Image.Area>
       <Image.Image 
-        urlImage={image}
+        urlImage={urlImage}
       />
     </Image.Area>
   )
